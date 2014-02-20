5ecead760591312c52b50fd620cfded3b8e0f990
atkay <a.t.kruzewski@gmail.com>
2014-02-20 17:15:42 +0000

    Added delete button

--- budgetHappens/AddEditWithdrawal.xaml.cs
+++ budgetHappens/AddEditWithdrawal.xaml.cs
@@ -50,6 +50,9 @@ namespace budgetHappens
                 TextBoxAmount.Text = selectedWithdrawal.Amount.ToString("0.00");
                 TextBoxDescription.Text = selectedWithdrawal.Description;
                 ButtonAddEditWithdrawal.Content = "Save";
+                ButtonAddEditWithdrawal.Width = 230;
+                ButtonAddEditWithdrawal.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                ButtonRemoveWithdrawal.Visibility = System.Windows.Visibility.Visible;
 
                 PhoneApplicationService.Current.State["SelectedWithdrawal"] = null;
             }
